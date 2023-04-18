6d8d22d6d1769625d2baf72ac9d51ac22ede9396
auronalidemaj <auronalidemaj@gmail.com>
2023-04-18 15:35:30 +0200

    ..

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Header from '../components/Header'
+import Slider from '../components/Slider'
 import Footer from '../components/footer'
 function Home() {
   return (
     <>
+    <Header/>
+    <Slider/>
     <Footer/>
     </>
   )
