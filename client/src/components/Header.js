866860c08f8f8326c9e193ed0c59d1bc935772d9
blerinakuka <kukajblerina@gmail.com>
2023-04-18 17:47:27 +0200

    header update

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { ReactComponent as Hamburger } from './logo.svg'
+import { ReactComponent as Brand } from './logo.svg'
+import './header.css'
 
-function Header() {
-  return (
-    <div>Header</div>
-  )
-}
-
-export default Header
+const Header = () => {
+    const [showNavbar, setShowNavbar] = useState(false)
+  
+    const handleShowNavbar = () => {
+      setShowNavbar(!showNavbar)
+    }
+  
+    return (
+      <nav className="navbar">
+        <div className="container">
+          <div className="logo">
+            <Brand/>
+            <logo className="emri">Bookstore</logo>
+          </div>
+          <div className="menu-icon" onClick={handleShowNavbar}>
+            <Hamburger />
+          </div>
+          <div className={`nav-elements  ${showNavbar && 'active'}`}>
+          <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/products">Products</NavLink>
+              </li>
+              <li>
+                <NavLink to="/cart">Cart</NavLink>
+              </li>
+              <li>
+                <NavLink to="/login">Login</NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+  
+  export default Header
