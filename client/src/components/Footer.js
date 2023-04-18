665ddd6d0704278d55b2bea9adf1da5bc865fa6c
auronalidemaj <auronalidemaj@gmail.com>
2023-04-18 15:22:11 +0200

    push

---
+++ client/src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function Footer() {
+  return (
+      <><footer className="top">
+          <img src="logo.svg" alt="Logo" />
+          <div className="links">
+              <div>
+                  <h2>Platform</h2>
+                  <a href="#">Directus Core</a>
+              </div>
+              <div>
+                  <h2>Cloud</h2>
+                  <a href="#">Directus Core</a>
+              </div>
+          </div>
+      </footer><footer className="bottom">
+              <div className="legal">
+                  <span>© 2023 All rights reserved</span>
+                  <a href="#">License</a>
+                  {/* more links */}
+              </div>
+              <div className="links">
+                  <a className="fa-brands fa-github" href="#"></a>
+                  <a className="fa-brands fa-linkedin" href="#"></a>
+                  <a className="fa-brands fa-docker" href="#"></a>
+              </div>
+          </footer></>
+
+  )
+}
+
+export default Footer
