57d58c9818a77b6cee66be55824104a36ed99f55
blerinakuka <kukajblerina@gmail.com>
2023-04-18 21:13:33 +0200

    navbar done

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,46 +1,48 @@
-import React from 'react'
-import { useState } from 'react'
-import { NavLink } from 'react-router-dom'
-import { ReactComponent as Hamburger } from './icons/logo.svg'
-import { ReactComponent as Brand } from './icons/logo.svg'
-import './style/header.css'
+import React from 'react';
+import { useState } from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { ReactComponent as Hamburger } from './icons/menu.svg';
+import { ReactComponent as Brand } from './icons/logo4.svg';
+import './style/header.css';
 
 const Header = () => {
-    const [showNavbar, setShowNavbar] = useState(false)
-  
-    const handleShowNavbar = () => {
-      setShowNavbar(!showNavbar)
-    }
-  
-    return (
-      <nav className="navbar">
-        <div className="container">
-          <div className="logo">
-            <Brand/>
-            <logo className="emri">Bookstore</logo>
-          </div>
-          <div className="menu-icon" onClick={handleShowNavbar}>
-            <Hamburger />
-          </div>
-          <div className={`nav-elements  ${showNavbar && 'active'}`}>
-          <ul>
-              <li>
-                <NavLink to="/">Home</NavLink>
-              </li>
-              <li>
-                <NavLink to="/products">Products</NavLink>
-              </li>
-              <li>
-                <NavLink to="/cart">Cart</NavLink>
-              </li>
-              <li>
-                <NavLink to="/login">Login</NavLink>
-              </li>
-            </ul>
-          </div>
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo">
+          <Brand />
+          <p className="emri">Bookstore</p>
         </div>
-      </nav>
-    )
-  }
-  
-  export default Header
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          <Hamburger />
+        </div>
+        <div className={`nav-elements  ${showNavbar && 'active'}`}>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/products">Products</NavLink>
+            </li>
+            <li>
+              <NavLink to="/cart">Cart</NavLink>
+            </li>
+            <li>
+              <Link to="/login">
+                <button className="nav-login-btn">Login</button>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
