a1580f4db3787f64efb5ba9e1d5fadb721a6a9e7
auronalidemaj <auronalidemaj@gmail.com>
2023-04-18 17:45:23 +0200

    Footer completed

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,33 +1,81 @@
-import React from 'react'
-import './style/footer.css'
+import React from 'react';
+import './style/footer.css';
+import fb from './icons/facebook.png'
+import twitter from './icons/twitter.png'
+import instagram from './icons/instagram.png'
+import linkedin from './icons/linkedin.png'
+
 function footer() {
   return (
-      <><footer className="top">
-          <img src="logo.svg" alt="Logo" />
-          <div className="links">
-              <div>
-                  <h2>Platform</h2>
-                  <a href="#">Directus Core</a>
+    <>
+      <div className="footer">
+        <div className="sb__footer section__padding">
+          <div className="sb__footer-links">
+            <div className="sb__footer-links_div">
+              <h4>Bookstore</h4>
+              <a href="/employer">
+                <p>Employer</p>
+              </a>
+              <a href="/health">
+                <p>Health</p>
+              </a>
+              <a href="/absd">
+                <p>Absd</p>
+              </a>
+            </div>
+            <div className="sb__footer-links_div">
+              <h4>Resources</h4>
+              <a href="/employer">
+                <p>Employer</p>
+              </a>
+              <a href="/employer">
+                <p>Employer</p>
+              </a>
+              <a href="/employer">
+                <p>Employer</p>
+              </a>
+              
+            </div>
+            <div className="sb__footer-links_div">
+              <h4>Resources</h4>
+              <a href="/employer">
+                <p>Employer</p>
+              </a>
+              <a href="/employer">
+                <p>Employer</p>
+              </a>
+              <a href="/employer">
+                <p>Employer</p>
+              </a>
+            </div>
+            <div className="sb__footer-links_div">
+              <h4>Social Media</h4>
+              <div className="socialmedia">
+                <p><img src={fb} alt=""/></p>
+                <p><img src={twitter} alt=""/></p>
+                <p><img src={instagram} alt=""/></p>
+                <p><img src={linkedin} alt=""/></p>
               </div>
-              <div>
-                  <h2>Cloud</h2>
-                  <a href="#">Directus Core</a>
-              </div>
+            </div>
           </div>
-      </footer><footer className="bottom">
-              <div className="legal">
-                  <span>© 2023 All rights reserved</span>
-                  <a href="#">License</a>
-                  {/* more links */}
-              </div>
-              <div className="links">
-                  <a className="fa-brands fa-github" href="#"></a>
-                  <a className="fa-brands fa-linkedin" href="#"></a>
-                  <a className="fa-brands fa-docker" href="#"></a>
-              </div>
-          </footer></>
-
-  )
+          <hr></hr>
+          <div className="sb__footer-below">
+            <div className="sb__footer-copyright">
+              <p>
+                @{new Date().getFullYear()} Bookstore. All rights reserved.
+              </p>
+            </div>
+            <div className="sb__footer-below-links">
+              <a href="/terms"><div><p>Terms and Conditions</p></div></a>
+              <a href="/terms"><div><p>Terms and Conditions</p></div></a>
+              <a href="/terms"><div><p>Terms and Conditions</p></div></a>
+              <a href="/terms"><div><p>Terms and Conditions</p></div></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default footer
+export default footer;
