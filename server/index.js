45cd90d473e2c6bad289594fc4e42c7c9c77583d
auronalidemaj <auronalidemaj@gmail.com>
2023-04-20 16:10:10 +0200

    comm

--- server/index.js
+++ server/index.js
@@ -1,18 +1,39 @@
-// import express from "express"
-const mysql = require('mysql')
+const express = require("express")
+const mysql = require("mysql")
+const cors = require("cors");
 
-// const app = express()
-
-// app.listen(3000, ()=>{})
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 const db = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"",
-    database:"bookstore"
+    password:"password",
+    database:"loginsystem"
 });
 
-db.connect((err)=>{
-    if(err) throw err;
-    console.log("Connected to mysql database");
+// Open a MySQL connection
+db.connect((err) => {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + db.threadId);
+});
+
+app.post("/register", (req, res)=> {
+    const username = req.body.username;
+    const password = req.body.password;
+    db.query("INSERT INTO users(username, password) VALUES (?,?)", [username,password], (err,result) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send(err);
+        }
+        res.send("User registered successfully!");
+    });
+});
+
+app.listen(3001,() => {
+    console.log("running server");
 });
